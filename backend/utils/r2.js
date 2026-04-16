6d4fd48519361cc32b190625b79bde73b93115f6
cheblichebli <admin@3xc-gaming.com>
2026-04-16 04:36:36 +0100

    Create r2.js

---
+++ backend/utils/r2.js
@@ -0,0 +1,29 @@
+const { S3Client, PutObjectCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
+
+const r2 = new S3Client({
+  region: 'auto',
+  endpoint: process.env.R2_ENDPOINT,
+  credentials: {
+    accessKeyId: process.env.R2_ACCESS_KEY_ID,
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+  },
+});
+
+const uploadToR2 = async (key, buffer, contentType) => {
+  await r2.send(new PutObjectCommand({
+    Bucket: process.env.R2_BUCKET_NAME,
+    Key: key,
+    Body: buffer,
+    ContentType: contentType,
+  }));
+  return `${process.env.R2_PUBLIC_URL}/${key}`;
+};
+
+const deleteFromR2 = async (key) => {
+  await r2.send(new DeleteObjectCommand({
+    Bucket: process.env.R2_BUCKET_NAME,
+    Key: key,
+  }));
+};
+
+module.exports = { uploadToR2, deleteFromR2 };
